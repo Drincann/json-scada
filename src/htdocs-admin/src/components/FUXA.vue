c7fded0d4ebf037f9394820cb366b7c1dad773c7
drincann <1019933576@qq.com>
2021-07-19 11:56:18 +0800

    fix: admin 中集成的 FUXA 窗口大小自动调整行为异常

--- src/htdocs-admin/src/components/FUXA.vue
+++ src/htdocs-admin/src/components/FUXA.vue
@@ -3,7 +3,7 @@
     src="http://localhost:1881"
     height="auto"
     width="100%"
-    onload="this.height = document.body.offsetHeight - 110; window.addEventListener('resize', () => this.height = document.body.offsetHeight - 110);"
+    onload="this.height = document.documentElement.clientHeight - 110; window.addEventListener('resize', () => this.height = document.documentElement.clientHeight - 110);"
   ></iframe>
 </template>
 
