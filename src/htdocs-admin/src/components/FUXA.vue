1f578c8412359f7d387d781bfa86f22ace7d56d6
drincann <1019933576@qq.com>
2021-07-27 10:33:32 +0800

    fix(htdoc-admin): FUXA component 错误地访问了本地应用

--- src/htdocs-admin/src/components/FUXA.vue
+++ src/htdocs-admin/src/components/FUXA.vue
@@ -1,15 +1,17 @@
 <template>
   <iframe
-    src="http://localhost:1881"
+    :src="src"
     height="auto"
     width="100%"
-    onload="this.height = document.documentElement.clientHeight - 110; window.addEventListener('resize', () => this.height = document.documentElement.clientHeight - 110);"
+    onload="this.height = document.documentElement.clientHeight - 120; window.addEventListener('resize', () => this.height = document.documentElement.clientHeight - 120);"
   ></iframe>
 </template>
 
 <script>
 export default {
   name: "FUXA",
-  data: () => ({}),
+  data: () => ({
+    src: window.location.protocol + "//" + window.location.hostname + ":1881",
+  }),
 };
 </script>
