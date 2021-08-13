5d28de8edf8a69e53ad876943795099f9a764abb
drincann <1019933576@qq.com>
2021-08-13 15:19:28 +0800

    refactor: 现在 svg 文件管理 api server 将不包含构建结果

--- src/server_realtime_auth/svgManagerApiServer/dist/util/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-exports.env = {
-    SvgFolder: (_a = process.env['SvgFolder']) !== null && _a !== void 0 ? _a : '/htdocs/svg',
-};
